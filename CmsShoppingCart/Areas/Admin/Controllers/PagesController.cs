b14b7ae53a9c64417166e0a97a400e9145d32efd
sroy1021 <roysanjay226@gmail.com>
2019-11-07 11:49:09 +0530

    Admin Pages 2

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,33 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageVM
+            List<PageVM> pages;
+
+
+            //Init the List
+
+            using (Db db = new Db())
+            {
+                pages = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return view with list
+
+
+            return View(pages);
+        }
+    }
+}
